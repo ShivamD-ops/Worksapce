5f54f5f559c058608f6f3d3fff1dff5c992a1500
SHIVAM DEVASER <shivam.devaser2@gmail.com>
2024-11-25 10:54:13 +0530

    day-6-start

---
+++ python-files/day-6/basics.py
@@ -0,0 +1,7 @@
+print("hello")
+print(8+489092)
+#coment single line
+''' multiple
+line 
+comment
+'''
---
+++ python-files/day-6/variables.py
@@ -0,0 +1,5 @@
+number = 1 #int
+print(number)
+print(type(number))
+name = "Shivam" #string
+print(type(name))
